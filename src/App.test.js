86bf2e1e5a4e64e0466e429d15a0dd686434856f
MMcClure11 <mcclme02@luther.edu>
2021-01-26 15:08:54 -0600

    Add notes on tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,10 +1,12 @@
-import Enzyme from 'enzyme';
+import Enzyme, { shallow } from 'enzyme';
 import EnzymeAdapter from '@wojtekmaj/enzyme-adapter-react-17';
 
 import App from './App';
 
 Enzyme.configure({ adapter: new EnzymeAdapter() });
 
-test('renders learn react link', () => {
-
+test('renders without crashing', () => {
+  const wrapper = shallow(<App />)
+  // console.log(wrapper.debug()); //helpful for debugging, returns the DOM as a string
+  // expect(wrapper).toBeTruthy();
 });
